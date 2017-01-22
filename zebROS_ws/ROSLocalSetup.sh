783b00a7c5b749ef1773d234608628cde516a1c0
Adithya Balaji <adithyabsk@gmail.com>
2017-01-21 20:59:17 -0500

    Add Local ROS setup File

--- zebROS_ws/ROSLocalSetup.sh
+++ zebROS_ws/ROSLocalSetup.sh
@@ -2,7 +2,7 @@
 
 source /opt/ros/kinetic/setup.bash
 source ~/2017VisionCode/zebROS_ws/devel/setup.bash
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_IP=$(hostname -I)
+export ROS_MASTER_URI=http://10.9.0.35:11311
+export ROS_IP=10.9.0.35
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
