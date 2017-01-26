749cc38f40d1671659f860757a42c6c82776af16
Adithya Balaji <adithyabsk@gmail.com>
2017-01-25 19:35:58 -0500

    Untrack build and devel directory files

--- zebROS_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/ubuntu/2017VisionCode/zebROS_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/ubuntu/2017VisionCode/zebROS_ws/devel;/opt/ros/kinetic".split(';') if "/home/ubuntu/2017VisionCode/zebROS_ws/devel;/opt/ros/kinetic" != "" else []
--- zebROS_ws/build/goal_detection/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/2016VisionCode/zebROS_ws/src/goal_detection
-# Build directory: /home/ubuntu/2016VisionCode/zebROS_ws/build/goal_detection
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
