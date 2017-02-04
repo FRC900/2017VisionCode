0dc8df009ff16d0354af33fc8ae76efa89b90967
Alexander Allen <admin@altechcode.com>
2017-02-04 16:06:44 -0500

    Fixed ROS hooks for localization

--- zebROS_ws/src/localization/src/localization.cpp
+++ zebROS_ws/src/localization/src/localization.cpp
@@ -28,9 +28,9 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "localization");
 
 	ros:NodeHandle nh;
-	message_filters::Subscriber<sensor_msgs::Imu> imu(nh, "/navx/imu", 1);
-	message_filters::Subscriber<nav_msgs::Odometry> odom(nh, "/navx/odom", 1);
-	message_filters::Subscriber<navx_publisher::stampedUInt64> time(nh, "/navx/time", 1);
+	message_filters::Subscriber<sensor_msgs::Imu> imu(nh, "/navx/imu", 10);
+	message_filters::Subscriber<nav_msgs::Odometry> odom(nh, "/navx/odom", 10);
+	message_filters::Subscriber<navx_publisher::stampedUInt64> time(nh, "/navx/time", 10);
 
 	typedef sync_policies::ApproximateTime<sensor_msgs::Imu,odom,_msg,navx_publisher::stampedUInt64> syncPolicy;
 	Synchronizer<syncPolicy> sync(syncPolicy(50), imu, odom, time);
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
 
 	localization::LocalizationMessage local_msg;
 
-	pub = nh.advertise<localization::LocalizationMessage>("localization_msg", 1);
+	pub = nh.advertise<localization:LocalizationMessage>("/localization/pos", 10);
 
 	ros::spin();
 
