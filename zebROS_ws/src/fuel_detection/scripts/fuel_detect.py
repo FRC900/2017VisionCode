29410a2796a8edb6ba236ee70d36c0024e7902ba
Adithya Balaji <adithyabsk@gmail.com>
2017-02-08 20:32:44 -0500

    Refine fuel_detect.py and update ROS functions

--- zebROS_ws/src/fuel_detection/scripts/fuel_detect.py
+++ zebROS_ws/src/fuel_detection/scripts/fuel_detect.py
@@ -28,8 +28,8 @@ import sys
 
 class BlobDetector:
     def __init__(self):
-        self.isTesting = False
-	self.hl         = 20
+        self.isTesting  = False
+        self.hl         = 20
         self.sl         = 137
         self.vl         = 80
        	self.hu         = 149
@@ -43,13 +43,14 @@ class BlobDetector:
         self.param2     = 48
         self.min_radius = 5
         self.max_radius = 27
+
         if len(sys.argv) == 2:
             def nothing(x):
                 pass
             self.isTesting = True
-        self.contour_image = np.zeros((720, 1280, 3), np.uint8)
-        self.hough_image   = np.zeros((720, 1280, 3), np.uint8)
-            
+            self.contour_image = np.zeros((720, 1280, 3), np.uint8)
+            self.hough_image   = np.zeros((720, 1280, 3), np.uint8)
+
             # Create HSV tracker Window
             cv2.namedWindow('HSV')
             cv2.createTrackbar('HL', 'HSV', self.hl, 180, nothing)
@@ -70,20 +71,19 @@ class BlobDetector:
             cv2.createTrackbar('MAX_RADIUS', 'HOUGH', self.max_radius, 100, nothing)
 
             self.test = self.window_runner() 
-        
+
         self.bridge = CvBridge()
         self.pub_blobs = rospy.Publisher("/fuels", PointCloud, queue_size=1)
         self.sub_image = message_filters.Subscriber("/zed/rgb/image_raw_color", Image, queue_size=1)
         self.sub_depth = message_filters.Subscriber("/zed/depth/depth_registered", Image, queue_size=1)
         self.ts = message_filters.TimeSynchronizer([self.sub_image, self.sub_depth], 10)
         self.ts.registerCallback(self.processImage)
-        
-        rospy.loginfo("FuelDetector initialized.")
+
+        rospy.loginfo("Fuel detector initialized.")
 
     def processImage(self, image_msg, depth_msg):
         im = self.bridge.imgmsg_to_cv2(image_msg) # Convert image to cv mat using CVBridge
-        # im = im[:.4*len(im)]
-    
+
         hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV) # convert color space of image to HSV
         self.msg = PointCloud() # Initialize message to store ball types
 
@@ -92,42 +92,42 @@ class BlobDetector:
             self.pub_blobs.publish(self.msg)
         else:
             self.find_color(im, depth_msg, cv2.inRange(hsv, np.array([self.hl, self.sl, self.vl]), np.array([self.hu, self.su, self.vu])))
-    
+
     def screenToWorld(self, obj_rect, depth, fov_size, frame_size, camera_elavation):
         _depth_obj = 0.127
         rect_center = tuple([obj_rect[0] + obj_rect[2]/2., obj_rect[1] + obj_rect[3]/2.])
         dist_to_center = tuple([rect_center[0] - (frame_size[0]/2.), -1*rect_center[1] + frame_size[1]/2.])
-        
+
         azimuth = math.atan(dist_to_center[0] / (0.5 * frame_size[0] / math.tan(fov_size[0] / 2.)))
         inclination = math.atan(dist_to_center[1] / (0.5 * frame_size[1] / math.tan(fov_size[1] / 2.)))
-        
+
         depth += _depth_obj / 2.
-        
+
         point_3d = (depth * math.cos(inclination) * math.sin(azimuth),
                     depth * math.cos(inclination) * math.cos(azimuth),
                     depth * math.sin(inclination))
         return point_3d
-        
-    
-    
+
+
+
     def find_color(self, passed_im, depth_msg, mask):
         contour_image = passed_im.copy() # Make a copy of image to modify
         if self.isTesting:
             self.contour_image = contour_image
-        
+
         # Perform morphology "open" which performs dilate and expand with a kernal size of 3
         k_size = 3
         kernel = np.ones((k_size,k_size),np.uint8)
         mask   = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        
+
         # Find Contours on mask and process contours
         contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1] # Find contours on masked iamge
         points = []
         approx_contours = []
         for c in contours:
             area = cv2.contourArea(c)
-	    if area < self.area_limit: continue
-            
+            if area < self.area_limit: continue
+
             # Circles and blobs of circles should beat this condition
             epsilon = 0.005*cv2.arcLength(c,True)
             approx = cv2.approxPolyDP(c,epsilon,True)
@@ -166,24 +166,22 @@ class BlobDetector:
         # cv2.imshow('CONTOUR_MASK', mask2)
 
         # Mask original image and create a grayscale image for hough circles
-        self.hough_image = cv2.bitwise_and(self.contour_image, self.contour_image, mask=mask2) # flip to mask if necessary ()
-        gray = cv2.cvtColor(cv2.cvtColor(self.hough_image, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
+        hough_image = cv2.bitwise_and(contour_image, contour_image, mask=mask2) # flip to mask if necessary ()
+        gray = cv2.cvtColor(cv2.cvtColor(hough_image, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY)
 
         # Find hough circles 
-        # dp=3, minDist=20, param1=500, param2=50, minRadius=5, maxRadius=20
         circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=self.dp, 
-                                      minDist=self.min_dist, param1=self.param1, param2=self.param2, 
-                                      minRadius=self.min_radius, maxRadius=self.max_radius)
+                                   minDist=self.min_dist, param1=self.param1, param2=self.param2, 
+                                   minRadius=self.min_radius, maxRadius=self.max_radius)
         
         if circles is not None:
-            # convert the (x, y) coordinates and radius of the circles to integers
             circles = np.round(circles[0, :]).astype("int")
-            # loop over the (x, y) coordinates and radius of the circles
-            for (x, y, r) in circles:
-                # draw the circle in the output image, then draw a rectangle
-                # corresponding to the center of the circle
-                cv2.circle(self.hough_image, (x, y), r, (0, 255, 0), 4)
-                cv2.rectangle(self.hough_image, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)         
+            if self.isTesting:
+                for (x, y, r) in circles:
+                    # draw the circle in the output image, then draw a rectangle
+                    # corresponding to the center of the circle
+                    cv2.circle(hough_image, (x, y), r, (0, 255, 0), 4)
+                    cv2.rectangle(hough_image, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)         
 
         for i, point in enumerate(points):
             point = Point32()
@@ -196,10 +194,11 @@ class BlobDetector:
             # self.msg.heights.append(float((max(approx, key=lambda x: x[0][1])[0][1] - min(approx, key=lambda x: x[0][1])[0][1])) / len(im))
             # cv2.putText(im, label_color, center, cv2.FONT_HERSHEY_PLAIN, 2, (100, 255, 100))
             # print "Label color:  {}".format(label_color)
-        
-        if points:
-            if self.isTesting:
-                cv2.drawContours(self.image, approx_contours, -1, (100, 255, 100), 2)
+
+        if self.isTesting and points:
+            cv2.drawContours(self.contour_image, approx_contours, -1, (100, 255, 100), 2)
+        if self.isTesting:
+            self.hough_image = hough_image
     def window_runner(self):
         while(True):
             cv2.imshow('HSV', cv2.resize(self.contour_image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA))
@@ -225,4 +224,3 @@ if __name__ == "__main__":
     bd = BlobDetector()
     rospy.spin()
     cv2.destroyAllWindows()
-            
