a900d6e9d5800086a976cd43da7dc124044f601b
Team900 Jetson TX1 <progammers@team900.org>
2017-02-10 22:40:05 +0000

    Jetson to Jetson comms setup files

---
+++ zebROS_ws/ROSJetsonSlave.sh
@@ -0,0 +1,8 @@
+# Setup ROS for Local Development
+
+source /opt/ros/kinetic/setup.bash
+source ~/2017VisionCode/zebROS_ws/devel/setup.bash
+export ROS_MASTER_URI=http://10.9.0.11:11311
+export ROS_IP=10.9.0.12
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
