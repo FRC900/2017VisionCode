89f82473e4fe3c6295379fb74ee781930b9cd61b
Team900 Jetson TX1 <progammers@team900.org>
2017-02-10 19:59:18 -0500

    Fix ROS Setup Files

--- zebROS_ws/ROSJetsonMaster.sh
+++ zebROS_ws/ROSJetsonMaster.sh
@@ -1,8 +1,10 @@
-# Setup ROS for Local Development
+#!/usr/bin/env bash
 
+# Setup ROS for Jetson Master
 source /opt/ros/kinetic/setup.bash
 source ~/2017VisionCode/zebROS_ws/devel/setup.bash
 export ROS_MASTER_URI=http://10.9.0.11:11311
 export ROS_IP=10.9.0.11
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
+exec "$@"
--- zebROS_ws/ROSRioSetup.sh
+++ zebROS_ws/ROSRioSetup.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
+
 # Setup Rio ROS Environment
-
-# Setup ROS for Local Development
-
 source /opt/ros/kinetic/setup.bash
 source ~/2017VisionCode/zebROS_ws/devel/setup.bash
 export ROS_MASTER_URI=http://10.9.0.2:11311
-export ROS_IP=10.9.0.35
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH=
+export ROS_IP=10.9.0.12
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+exec "$@"
