6efe69d93b1f8bc348b78696020f9709b61ddca1
Team900 Jetson TX1 <progammers@team900.org>
2017-02-11 16:54:17 -0500

    Update IP for ROS Setup

--- zebROS_ws/ROSJetsonMaster.sh
+++ zebROS_ws/ROSJetsonMaster.sh
@@ -3,8 +3,8 @@
 # Setup ROS for Jetson Master
 source /opt/ros/kinetic/setup.bash
 source ~/2017VisionCode/zebROS_ws/devel/setup.bash
-export ROS_MASTER_URI=http://10.9.0.11:11311
-export ROS_IP=10.9.0.11
+export ROS_MASTER_URI=http://10.9.0.8:11311
+export ROS_IP=10.9.0.8
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 exec "$@"
--- zebROS_ws/ROSJetsonSlave.sh
+++ zebROS_ws/ROSJetsonSlave.sh
@@ -3,7 +3,7 @@
 # Setup ROS for Jetson Slave
 source /opt/ros/kinetic/setup.bash
 source ~/2017VisionCode/zebROS_ws/devel/setup.bash
-export ROS_MASTER_URI=http://10.9.0.11:11311
+export ROS_MASTER_URI=http://10.9.0.8:11311
 export ROS_IP=10.9.0.12
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
