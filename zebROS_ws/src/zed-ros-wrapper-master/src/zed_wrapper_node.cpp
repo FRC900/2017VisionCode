119b3d7db5a7a9ec932bf1f861140dc3b809982f
Team900 Jetson TX1 <progammers@team900.org>
2017-02-14 20:18:23 -0500

    Potential node crashing issue fix

--- zebROS_ws/src/zed-ros-wrapper-master/src/zed_wrapper_node.cpp
+++ zebROS_ws/src/zed-ros-wrapper-master/src/zed_wrapper_node.cpp
@@ -20,8 +20,10 @@
 
 #include <ros/ros.h>
 #include <nodelet/loader.h>
+#include <X11/Xlib.h>
 
 int main(int argc, char** argv) {
+	XInitThreads();
     ros::init(argc, argv, "zed_wrapper_node");
 
     nodelet::Loader nodelet;
