cfd470bcccc6528a9a09af840266b863b6773697
Team900 Jetson TX1 <progammers@team900.org>
2017-02-21 22:08:49 -0500

    Fixed recording launch files

--- launch/slave.sh
+++ launch/slave.sh
@@ -2,4 +2,9 @@
 
 . /home/ubuntu/2017VisionCode/zebROS_ws/ROSJetsonSlave.sh
 
-roslaunch controller_node controller_slave.launch
+if sudo mount /dev/disk/by-id/$(ls /dev/disk/by-id/ | grep 'SanDisk.*part1') /mnt/900_2; then
+	sudo chmod a+rw /mnt/900_2/
+	roslaunch controller_node controller_slave.launch record:=true
+else
+	roslaunch controller_node controller_slave.launch
+fi
