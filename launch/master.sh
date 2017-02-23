700e1b71c83c713b8dfef3d5f26f3603475d46f2
Team900 Jetson TX1 <progammers@team900.org>
2017-02-22 20:54:42 -0500

    Reorganized systemd unit files

---
+++ launch/master.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. ./home/ubuntu/2017VisionCode/zebROS_ws/ROSJetsonMaster.sh
+
+roslaunch controller_node controller_master.launch
---
+++ launch/slave.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. ./home/ubuntu/2017VisionCode/zebROS_ws/ROSJetsonSlave.sh
+
+roslaunch controller_node controller_slave.launch
