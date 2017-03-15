29b52eb744dea293267b8bab88ec9a8dc59639a5
Team900 Jetson TX1 <progammers@team900.org>
2017-03-15 18:08:25 -0400

    Try to record on every restart, load IPv6 disable bits

--- launch/master.sh
+++ launch/master.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Reload IPv6 networking blocks
+sudo sysctl -p
+
 . /home/ubuntu/2017VisionCode/zebROS_ws/ROSJetsonMaster.sh
 #echo 1100-1200,443,80,554,1735 > /proc/sys/net/ipv4/ip_local_reserved_ports
 
@@ -8,6 +11,7 @@
 
 #sudo chmod a+rw /dev/ttyACM0
 
+sudo umount /mnt/900_2
 if sudo mount /dev/disk/by-id/$(ls /dev/disk/by-id/ | grep 'SanDisk.*part1') /mnt/900_2; then
 		sudo chmod a+rw /mnt/900_2/
 		roslaunch controller_node controller_master.launch record:=true
