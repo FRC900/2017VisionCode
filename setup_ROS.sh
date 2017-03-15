e1faa8a993cfa06ac45f4a788b9560c6086288ad
Team900 Jetson TX1 <progammers@team900.org>
2017-03-15 18:06:24 -0400

    Add extra ROS packages

--- setup_ROS.sh
+++ setup_ROS.sh
@@ -20,8 +20,9 @@ sudo apt install ros-kinetic-ros-base -y
 # To find available packages:
 # apt-cache search ros-kinetic
 # 
+sudo apt install python-rosdep python-rosinstall terminator ros-kinetic-rqt ros-kinetic-rqt-common-plugins ros-kinetic-tf2-ros ros-kinetic-pcl-conversions ros-kinetic-cv-bridge ros-kinetic-tf ros-kinetic-map-server ros-kinetic-rviz ros-kinetic-hector-slam ros-kinetic-hector-slam-launch ros-kinetic-rtabmap-ros ros-kinetic-robot-localization ros-kinetic-navigation
+
 # Initialize rosdep
-sudo apt-get install python-rosdep -y
 # ssl certificates can get messed up on TX1 for some reason
 sudo c_rehash /etc/ssl/certs
 # Initialize rosdep
@@ -31,6 +32,3 @@ rosdep update
 
 source /opt/ros/kinetic/setup.bash
 
-sudo apt install python-rosinstall -y
-sudo apt install terminator
-sudo apt install ros-kinetic-rqt ros-kinetic-rqt-common-plugins ros-kinetic-tf2-ros ros-kinetic-pcl-conversions ros-kinetic-cv-bridge ros-kinetic-tf ros-kinetic-map-server ros-kinetic-rviz ros-kinetic-hector-slam ros-kinetic-hector-slam-launch
