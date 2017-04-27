112c17d96087e7e6d65f1f6f20fd9094ff6f047e
Kevin Jaget <kjaget@nc.rr.com>
2017-04-27 10:00:42 -0400

    Start of Google Cartographer integration

--- setup_ROS.sh
+++ setup_ROS.sh
@@ -20,7 +20,7 @@ sudo apt install ros-kinetic-ros-base -y
 # To find available packages:
 # apt-cache search ros-kinetic
 # 
-sudo apt install python-rosdep python-rosinstall terminator ros-kinetic-rqt ros-kinetic-rqt-common-plugins ros-kinetic-tf2-ros ros-kinetic-pcl-conversions ros-kinetic-cv-bridge ros-kinetic-tf ros-kinetic-map-server ros-kinetic-rviz ros-kinetic-hector-slam ros-kinetic-hector-slam-launch ros-kinetic-rtabmap-ros ros-kinetic-robot-localization ros-kinetic-navigation ros-kinetic-robot-state-publisher
+sudo apt install python-rosdep python-rosinstall terminator ros-kinetic-rqt ros-kinetic-rqt-common-plugins ros-kinetic-tf2-ros ros-kinetic-pcl-conversions ros-kinetic-cv-bridge ros-kinetic-tf ros-kinetic-map-server ros-kinetic-rviz ros-kinetic-hector-slam ros-kinetic-hector-slam-launch ros-kinetic-rtabmap-ros ros-kinetic-robot-localization ros-kinetic-navigation ros-kinetic-robot-state-publisher python-wstool ninja-build
 
 # Initialize rosdep
 # ssl certificates can get messed up on TX1 for some reason
@@ -29,6 +29,19 @@ sudo c_rehash /etc/ssl/certs
 sudo rosdep init
 # To find available packages, use:
 rosdep update
+
+# Google Cartographer installation from https://google-cartographer-ros.readthedocs.io/en/latest/
+# (Latest as of 4/27/2017, at least)
+cd ~/2017VisionCode/zebROS_ws
+# Merge the cartographer_ros.rosinstall file and fetch code for dependencies.
+wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_ros/master/cartographer_ros.rosinstall
+wstool update -t src
+
+# Install deb dependencies.
+# The command 'sudo rosdep init' will print an error if you have already
+# executed it since installing ROS. This error can be ignored.
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
 
 source /opt/ros/kinetic/setup.bash
 
