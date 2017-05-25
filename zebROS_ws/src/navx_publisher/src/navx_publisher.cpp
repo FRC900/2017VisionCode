bdc7da2e6d3369217780ce83bd2770d6963ed32d
Kevin Jaget <kjaget@nc.rr.com>
2017-05-25 11:24:20 -0400

    Assorted cleanup

--- zebROS_ws/src/navx_publisher/src/navx_publisher.cpp
+++ zebROS_ws/src/navx_publisher/src/navx_publisher.cpp
@@ -16,13 +16,6 @@
 
 using namespace std;
 
-static ros::Publisher time_pub;
-static ros::Publisher imu_pub;
-static ros::Publisher odom_pub;
-static ros::Publisher raw_pub;
-
-#define PI 3.14159265
-
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "navx_publisher");
@@ -31,116 +24,33 @@ int main(int argc, char** argv)
 	// Set up publishers
 	// Raw_pub publishes in the ENU (east north up) orientation
 	// instead of NED (north east down)
-	time_pub = nh.advertise<navx_publisher::stampedUInt64>("/navx/time", 50);
-	imu_pub = nh.advertise<sensor_msgs::Imu>("/navx/imu", 50);
-	raw_pub = nh.advertise<sensor_msgs::Imu>("/navx/raw", 50);
-	odom_pub = nh.advertise<nav_msgs::Odometry>("/navx/odom", 50);
+	ros::Publisher time_pub = nh.advertise<navx_publisher::stampedUInt64>("/navx/time", 50);
+	ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("/navx/imu", 50);
+	ros::Publisher raw_pub = nh.advertise<sensor_msgs::Imu>("/navx/raw", 50);
+	ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>("/navx/odom", 50);
 	ros::Rate loop_time(15);
 	navx_publisher::stampedUInt64 timestamp;
-	geometry_msgs::Quaternion orientation;
-	geometry_msgs::Vector3 linear_accel;
-	geometry_msgs::Vector3 linear_vel;
-	geometry_msgs::Vector3 angular_vel;
 	sensor_msgs::Imu imu_msg;
 	sensor_msgs::Imu imu_msg_raw;
 	nav_msgs::Odometry odom;
 
-
-	unsigned long long last_time = 0ULL;
-	tf::Quaternion last_rot (tf::Vector3(0.,0.,0.),0.);
-	tf::Quaternion rot;
-
-	bool firstrun = true;
-
-	AHRS nx("/dev/ttyACM0");
-	while(ros::ok()) {
-		timestamp.data = nx.GetLastSensorTimestamp();
-		timestamp.header.stamp = ros::Time::now();
-
-		//pull orientation data from NavX
-		orientation.x = nx.GetQuaternionX();
-		orientation.y = nx.GetQuaternionY();
-		orientation.z = nx.GetQuaternionZ();
-		orientation.w = nx.GetQuaternionW();
-		imu_msg.orientation = orientation;
-
-		orientation.x = nx.GetQuaternionY();
-		orientation.y = nx.GetQuaternionX();
-		orientation.z = -nx.GetQuaternionZ();
-		orientation.w = nx.GetQuaternionW();
-
-		imu_msg_raw.orientation = orientation;
+	imu_msg.linear_acceleration_covariance = {0,0,0,0,0,0,0,0,0};
+	imu_msg.angular_velocity_covariance = {0,0,0,0,0,0,0,0,0};
+	imu_msg.orientation_covariance = {0,0,0,0,0,0,0,0,0};
 
 
-		float grav = 9.81;
-		// Pull acceleration data from navx
-		linear_accel.x = nx.GetWorldLinearAccelX() * grav;
-		linear_accel.y = nx.GetWorldLinearAccelY() * grav;
-		linear_accel.z = nx.GetWorldLinearAccelZ() * grav;
+	imu_msg_raw.linear_acceleration_covariance = {0,0,0,0,0,0,0,0,0};
+	imu_msg_raw.angular_velocity_covariance = {0,0,0,0,0,0,0,0,0};
+	imu_msg_raw.orientation_covariance = {0,0,0,0,0,0,0,0,0};
 
-		imu_msg.linear_acceleration = linear_accel;
+	odom.twist.covariance = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	odom.pose.covariance = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-		double yaw = nx.GetYaw() * PI / 180;
-		double pitch = nx.GetPitch() * PI / 180;
-		double roll = nx.GetRoll() * PI / 180;
+	imu_msg_raw.header.frame_id = "navx_frame";
+	imu_msg.header.frame_id = "navx_frame";
+	odom.header.frame_id = "nav_current_frame";
 
-		//uncomment this to add gravity back into /navx/raw
-		//linear_accel.x = linear_accel.x + sin(roll)*cos(pitch)*grav;
-		//linear_accel.y = linear_accel.y + cos(roll)*sin(pitch)*grav;
-		//linear_accel.z = linear_accel.z + cos(pitch)*cos(roll)*grav;
-
-		imu_msg_raw.linear_acceleration = linear_accel;
-
-
-
-
-		linear_vel.x = nx.GetVelocityX();
-		linear_vel.y = nx.GetVelocityY();
-		linear_vel.z = nx.GetVelocityZ();
-
-
-
-		tf::Quaternion pose;
-		tf::quaternionMsgToTF(orientation, pose);
-		if(firstrun) last_rot = pose;
-		rot = pose * last_rot.inverse();
-		tf::Matrix3x3(rot).getRPY(roll,pitch,yaw);
-		float time = timestamp.data - last_time;
-		angular_vel.x = pitch / time;
-		angular_vel.y = -roll / time;
-		angular_vel.z = -yaw / time;
-		imu_msg.angular_velocity = angular_vel;
-		imu_msg_raw.angular_velocity = angular_vel;
-		last_rot = pose;
-		last_time = timestamp.data;
-
-		firstrun = false;
-
-		//pull position data (all this is is the integral of velocity so it's not very good)
-		odom.pose.pose.position.x = nx.GetDisplacementX();
-		odom.pose.pose.position.y = nx.GetDisplacementY();
-		odom.pose.pose.position.z = nx.GetDisplacementZ();
-		odom.pose.pose.orientation = orientation;
-
-		odom.twist.twist.linear = linear_vel;
-		odom.twist.twist.angular = angular_vel;
-
-		//set the header of the odometry structure
-		odom.header.stamp = ros::Time::now();
-		odom.header.frame_id = "nav_current_frame";
-
-		imu_msg.linear_acceleration_covariance = {0,0,0,0,0,0,0,0,0};
-		imu_msg.angular_velocity_covariance = {0,0,0,0,0,0,0,0,0};
-		imu_msg.orientation_covariance = {0,0,0,0,0,0,0,0,0};
-
-
-		imu_msg_raw.linear_acceleration_covariance = {0,0,0,0,0,0,0,0,0};
-		imu_msg_raw.angular_velocity_covariance = {0,0,0,0,0,0,0,0,0};
-		imu_msg_raw.orientation_covariance = {0,0,0,0,0,0,0,0,0};
-
-		odom.twist.covariance = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-		odom.pose.covariance = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
+	{
 		//read the file with covariances and apply it to the odometry and IMU
 		ifstream infile("/home/ubuntu/2017VisionCode/zebROS_ws/src/navx_publisher/navx_calib.dat");
 		if(!infile.good())
@@ -156,7 +66,6 @@ int main(int argc, char** argv)
 		ln = 0;
 		while(std::getline(infile, line))
 		{
-			
 			if(line == "") break;
 			imu_msg.angular_velocity_covariance[ln] = std::stod(line);
 			ln++;
@@ -164,7 +73,6 @@ int main(int argc, char** argv)
 		ln = 0;
 		while(std::getline(infile, line))
 		{
-			
 			if(line == "") break;
 			imu_msg.orientation_covariance[ln] = std::stod(line);
 			ln++;
@@ -172,22 +80,89 @@ int main(int argc, char** argv)
 		ln = 0;
 		while(std::getline(infile, line))
 		{
-			
 			if(line == "") break;
 			odom.twist.covariance[ln] = std::stod(line);
 			odom.pose.covariance[ln] = std::stod(line);
 			ln++;
 		}
+	}
+	imu_msg_raw.linear_acceleration_covariance = imu_msg.linear_acceleration_covariance;
+	imu_msg_raw.angular_velocity_covariance = imu_msg.angular_velocity_covariance;
+	imu_msg_raw.orientation_covariance = imu_msg.orientation_covariance;
 
-		imu_msg_raw.header.frame_id = "navx_base_frame";
-		imu_msg.header.frame_id = "navx_base_frame";
+	unsigned long long last_time = 0ULL;
+	tf::Quaternion last_rot (tf::Vector3(0.,0.,0.),0.);
+	tf::Quaternion rot;
 
-		imu_msg_raw.linear_acceleration_covariance = imu_msg.linear_acceleration_covariance;
-		imu_msg_raw.angular_velocity_covariance = imu_msg.angular_velocity_covariance;
-		imu_msg_raw.orientation_covariance = imu_msg.orientation_covariance;
+	bool firstrun = true;
 
-		imu_msg_raw.header.stamp = ros::Time::now();
-		imu_msg.header.stamp = ros::Time::now();
+	AHRS nx("/dev/ttyACM0");
+	while(ros::ok()) {
+		timestamp.data = nx.GetLastSensorTimestamp();
+
+		//set the timestamp for all headers
+		odom.header.stamp = 
+		imu_msg.header.stamp = 
+		imu_msg_raw.header.stamp = 
+		timestamp.header.stamp = ros::Time::now();
+
+		//pull orientation data from NavX
+		imu_msg.orientation.x = nx.GetQuaternionX();
+		imu_msg.orientation.y = nx.GetQuaternionY();
+		imu_msg.orientation.z = nx.GetQuaternionZ();
+		imu_msg.orientation.w = nx.GetQuaternionW();
+
+		imu_msg_raw.orientation.x = imu_msg.orientation.x;
+		imu_msg_raw.orientation.y = imu_msg.orientation.y;
+		imu_msg_raw.orientation.z = -imu_msg.orientation.z;
+		imu_msg_raw.orientation.w = imu_msg.orientation.w;
+
+		const double grav = 9.80665;
+		// Pull acceleration data from navx
+		imu_msg.linear_acceleration.x = nx.GetWorldLinearAccelX() * grav;
+		imu_msg.linear_acceleration.y = nx.GetWorldLinearAccelY() * grav;
+		imu_msg.linear_acceleration.z = nx.GetWorldLinearAccelZ() * grav;
+
+		double yaw = nx.GetYaw() * M_PI / 180.;
+		double pitch = nx.GetPitch() * M_PI / 180.;
+		double roll = nx.GetRoll() * M_PI / 180.;
+
+#if 1
+		imu_msg_raw.linear_acceleration = imu_msg.linear_acceleration;
+#else
+		//uncomment this to add gravity back into /navx/raw
+		imu_msg_raw.linear_acceleration.x = imu_msg.linear_acceleration.x + sin(roll)*cos(pitch)*grav;
+		imu_msg_raw.linear_acceleration.y = imu_msg.linear_acceleration.y + cos(roll)*sin(pitch)*grav;
+		imu_msg_raw.linear_acceleration.z = imu_msg.linear_acceleration.z + cos(pitch)*cos(roll)*grav;
+#endif
+
+		tf::Quaternion pose;
+		tf::quaternionMsgToTF(imu_msg_raw.orientation, pose); // or imu_msg? they differ in the z value
+		if(firstrun) last_rot = pose;
+		rot = pose * last_rot.inverse();
+		tf::Matrix3x3(rot).getRPY(roll,pitch,yaw);
+		const double time = timestamp.data - last_time;
+		imu_msg.angular_velocity.x = pitch / time;
+		imu_msg.angular_velocity.y = -roll / time;
+		imu_msg.angular_velocity.z = -yaw / time;
+		imu_msg_raw.angular_velocity = imu_msg.angular_velocity;
+		last_rot = pose;
+		last_time = timestamp.data;
+
+		firstrun = false;
+
+		//pull position data (all this is is the integral of velocity so it's not very good)
+		odom.pose.pose.position.x = nx.GetDisplacementX();
+		odom.pose.pose.position.y = nx.GetDisplacementY();
+		odom.pose.pose.position.z = nx.GetDisplacementZ();
+		odom.pose.pose.orientation = imu_msg_raw.orientation; // or imu_msg? see above question
+
+		odom.twist.twist.linear.x = nx.GetVelocityX();
+		odom.twist.twist.linear.y = nx.GetVelocityY();
+		odom.twist.twist.linear.z = nx.GetVelocityZ();
+
+		odom.twist.twist.angular = imu_msg.angular_velocity;
+
 		
 		//publish to ROS topics
 		time_pub.publish(timestamp);
